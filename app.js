97b5a0beb35cf9f90bdedbf76e7f55cea4a7f35b
TingboW <104877939+TingboW@users.noreply.github.com>
2022-07-09 16:23:19 -0700

    m3-assignment

--- app.js
+++ app.js
@@ -13,4 +13,6 @@ app.set('view engine', 'pug');
 
 app.use('/', routes);
 
+app.use(express.static('public'));
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
+const path = require('path');
+const auth = require('http-auth');
+
+const basic = auth.basic({
+    file: path.join(__dirname, '../users.htpasswd'),
+});
+
 const express = require('express');
-const router = express.Router();
+const mongoose = require ('mongoose');
 const {
     check,
     validationResult
 } = require('express-validator');
+
+const router = express.Router();
+const Registration = mongoose.model('Registration');
 
 router.get('/', function (req, res) {
     res.render('form', {
@@ -23,7 +33,13 @@ function (req, res) {
     //console.log(req.body);
     const errors = validationResult(req);
     if (errors.isEmpty()) {
-        res.send('Thank you for your registration!');
+        const registration = new Registration(req.body);
+        registration.save()
+            .then(() => {res.send('Thank you for your registration!'); })
+            .catch((err) => {
+                console.log(err);
+                res.send('Sorry! Something went wrong.');
+            });
     } else {
         res.render('form', {
             title: 'Registration form',
@@ -32,6 +48,14 @@ function (req, res) {
         });
     }
 });
+
+router.get('/registrations', basic.check((req, res) => {
+    Registration.find()
+        .then((registrations) => {
+            res.render('index', { title: 'Listing registrations', registrations });
+        })
+        .catch(() => { res.send('Sorry! Something went wrong.');});
+}));
 
 
 module.exports = router;
